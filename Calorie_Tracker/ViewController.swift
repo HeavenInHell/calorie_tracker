fdd08fab620fb84cb171be3e0b30fea89dc6fdda
Andrey Korotkov <andrey.korotkov@joy-dev.com>
2021-05-22 12:04:41 +0300

    Initial Commit

---
+++ Calorie_Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calorie_Tracker
+//
+//  Created by Эдуард on 22.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
